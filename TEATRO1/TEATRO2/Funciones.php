5db5f9e34eee5f28b60f617b318ac5dd256b8a50
Aluhe22 <74068477+Aluhe22@users.noreply.github.com>
2021-04-12 14:39:23 -0300

    Create Funciones.php

---
+++ TEATRO1/TEATRO2/Funciones.php
@@ -0,0 +1,75 @@
+<?php
+class Funciones{
+    private $nombre;
+    private $precio;
+    private $horaInicio;
+    private $duracion;
+    
+    /**
+     * @param string $nombre
+     * @param double $precio
+     * @param string $horaInicio
+     * @param string $duracion
+     **/
+    public function __construct($nombre,$precio,$horaInicio,$duracion){
+        $this->nombre=$nombre;
+        $this->precio=$duracion;
+        $this->duracion=$duracion;
+        $this->horaInicio=$horaInicio;
+    }
+    /**
+     * @return string $nombre
+     **/
+    public function get_nombre(){
+        return $this->nombre;
+    }
+    /**
+     * @return double $precio
+     **/
+    public function get_precio(){
+        return $this->precio;
+    }
+    /**
+     * @return string $horaInicio
+     **/
+    public function get_horaInicio(){
+        return $this->horaInicio;
+    }
+    /**
+     * @return string $duracion
+     **/
+    public function get_duracion(){
+        return $this->duracion;
+    }
+    
+    /**
+     * @param string $nombreTeatro
+     **/
+    public function set_nombre($nombre){
+        $this->nombre=$nombre;
+    }
+    /**
+     * @param double $precio
+     **/
+    public function set_precio($precio){
+        $this->precio=$precio;
+    }
+    /**
+     * @param string $horaInicio
+     **/
+    public function set_horaInicio($horaInicio){
+        $this->horaInicio=$horaInicio;
+    }
+    /**
+     * @param string $duracion
+     **/
+    public function set_duracion($duracion){
+        $this->duracion=$duracion;
+    }
+    /**
+     * @return string $cadena
+     **/
+    public function __toString(){
+        return $cadena = "\nNombre: ".$this->get_nombre()."\nPrecio:$".$this->get_precio()."\nHora duracion: ".$this->get_horaInicio."\nDuracion: ".$this->get_duracion()."\n";
+    }
+}
