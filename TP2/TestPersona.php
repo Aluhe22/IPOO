46fbeb6d9540979168b083e80fa18605a56de14e
Aluhe22 <74068477+Aluhe22@users.noreply.github.com>
2021-04-12 16:08:47 -0300

    Update TestPersona.php

--- TP2/TestPersona.php
+++ TP2/TestPersona.php
@@ -1,7 +1,14 @@
 <?php
 use TP2\Persona;
+use TP2\CuentaBancaria;
 
 include 'Persona.php';
+include 'CuentaBancaria.php';
 
 $objPersona = new Persona("Micaela", "Martinez", "DNI", 41358725);
 echo $objPersona->__toString();
+
+$cadenap = $objPersona->__toString();
+
+$objBancario = new CuentaBancaria(1234, $cadenap , 10000, 0.12);
+echo $objBancario->__toString();
